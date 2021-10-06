864886a8964abfd2dfecc2a5ecddb06b46540ab9
Malik <79592097+havaian@users.noreply.github.com>
2021-10-06 09:06:26 +0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const scrollable = document.querySelector('.main-content');
 
 let current = 0;
 let target = 0;
-let ease = 0.05;
+let ease = 0.15;
 
 // Linear inetepolation used for smooth scrolling and image offset uniform adjustment
 
